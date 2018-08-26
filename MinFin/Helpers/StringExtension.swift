0e8886991fb225ffe13b493b9d005cd50cff758c
Владимир Мельников <vlmlnkv@gmail.com>
2018-08-26 16:28:47 +0400

    Added localization

---
+++ MinFin/Helpers/StringExtension.swift
@@ -0,0 +1,9 @@
+//
+//  StringExtension.swift
+//  MinFin
+//
+//  Created by Владимир Мельников on 26/08/2018.
+//  Copyright © 2018 Владимир Мельников. All rights reserved.
+//
+
+import Foundation
---
+++ MinFin/Support/AppLanguage.swift
@@ -0,0 +1,57 @@
+//
+//  AppLanguage.swift
+//  MinFin
+//
+//  Created by Владимир Мельников on 26/08/2018.
+//  Copyright © 2018 Владимир Мельников. All rights reserved.
+//
+
+import Foundation
+
+class AppLanguage {
+    
+    static let lang = "lang"
+    static let history = "history"
+    static let library = "library"
+    static let about = "about"
+    static let language = "language"
+    static let back = "back"
+    static let history_title = "history_title"
+    static let state_heads = "state_heads"
+    static let ministers = "ministers"
+    static let events = "events"
+    static let multiple_ministers = "multiple_ministers"
+    static let multiple_governers = "multiple_governers"
+    static let search = "search"
+    static let headings = "headings"
+    static let catalog = "catalog"
+    static let addresses = "addresses"
+    static let write_us = "write_us"
+    static let ministry = "ministry"
+    static let cancel = "cancel"
+    
+    static let standart = AppLanguage()
+    private var languageKey = "appLanguageKey"
+    
+    var currentLanguage: String = "ru-RU"
+    
+    init() {
+        if let lang = UserDefaults.standard.string(forKey: languageKey) {
+            currentLanguage = lang
+        } else {
+            let lang = NSLocalizedString("lang", comment: "")
+            currentLanguage = lang
+            UserDefaults.standard.set(lang, forKey: languageKey)
+        }
+    }
+    
+    func changeLanguage() {
+        if currentLanguage == "ru-RU" {
+            UserDefaults.standard.set("en", forKey: languageKey)
+            currentLanguage = "en"
+        } else {
+            UserDefaults.standard.set("ru-RU", forKey: languageKey)
+            currentLanguage = "ru-RU"
+        }
+    }
+}
